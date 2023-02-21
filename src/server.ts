db32ff42407e02fd8f900f17f100da8d95dc86c2
Simão Neto <simaomenezesneto@gmail.com>
2023-02-21 13:04:35 +0000

    chore: setting eslint in application

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,13 @@ import fastify from 'fastify'
 const app = fastify()
 
 app.get('/hello', () => {
-    return 'Hello world'
+  return 'Hello world'
 })
 
-app.listen({
+app
+  .listen({
     port: 3333,
-}).then(() => {
+  })
+  .then(() => {
     console.log('HTTP Server Running!')
-})
+  })
